18377a18e09fec29c6d27c18d9194e14cd9d34d5
ucsaby <31402984+ucsaby@users.noreply.github.com>
2024-10-22 13:15:58 +0000

    add: import products to admin page table style: product table

--- js/admin.js
+++ js/admin.js
@@ -1,17 +1,52 @@
-const URL = "https://670fe58da85f4164ef2c6131.mockapi.io/products";
-
 const productsTableBody = document
   .getElementById("products-table")
   .querySelector("tbody");
 
 document.addEventListener("DOMContentLoaded", displayProducts);
 
-// function displayProducts() {
-//   fetch(URL)
-//     .then((response) => response.json())
-//     .then(
-//       (products) => (productsTableBody.innerHTML = products.map((product = "
-//         test
-//         ")))
-//     );
-// }
+const URL = "https://670fe58da85f4164ef2c6131.mockapi.io/products";
+
+function displayProducts() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then(
+      (products) =>
+        (productsTableBody.innerHTML = products
+          .map(
+            (product) => `
+            <tr>
+              <td>
+                <div class="admin-image-container"><img class="admin-image" src="../${product.imageURL}"></td></div>
+              </td>
+              <td>
+                <p class="admin-name">${product.name}</p>
+              </td>
+              <td>
+                <div class="admin-category-container"><p class="admin-category">${product.category}</p></div>
+              </td>
+              <td>
+                <p class="admin-price">${product.price} lei</p>
+              </td>
+              <td>
+                <p class="admin-promo">${product.promo}</p>
+              </td>
+              <td>
+                <p class="admin-details">${product.details}</p>
+              </td>
+              <td>
+                <p class="admin-feature">${product.feature1}</p>
+                <p class="admin-feature">${product.feature2}</p>
+                <p class="admin-feature">${product.feature3}</p>
+              </td>
+              <td>
+                <button><i class="material-icons-outlined">edit</i>edit</button>
+              </td>
+              <td>
+                <button><i class="material-icons-outlined">delete</i>delete</button>
+              </td>
+            </tr>
+          `
+          )
+          .join(""))
+    );
+}
